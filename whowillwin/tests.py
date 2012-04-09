b8cf14f90facf94f847310a0393d9ff0761249f1
JAC <lennykey@gmx.de>
2012-04-09 23:12:58 +0200

    Tests hinzugefügt. Eins um zu prüfen, ob die Funktion mannschaften ein Dictionary liefert. Das zweite prüft, ob Köln im Dictionary vorhanden ist. Beide Tests waren Ok

--- whowillwin/tests.py
+++ whowillwin/tests.py
@@ -1,6 +1,9 @@
+
+# _*_ coding: utf-8 _*_
 import unittest
 
 from pyramid import testing
+from views import mannschaften
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
@@ -10,7 +13,25 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_my_view(self):
-        from .views import my_view
+        from views import my_view
         request = testing.DummyRequest()
         info = my_view(request)
         self.assertEqual(info['project'], 'whoWillWin')
+        
+    def testMannschaften(self):
+        self.assertEqual(type( dict() ), type(mannschaften()), "Kein Dictionary zurueckgekommen")
+        
+    def testIstFCKoelnInDictionary(self):
+        #print mannschaften()
+        #print mannschaften().items()[0][1]
+        
+        #dieseMannschaften = []
+        #for mannschaftListe in (mannschaften().values()):
+        #        for mannschaft in mannschaftListe:
+        #            dieseMannschaften.append( str(mannschaft).encode("utf-8").decode("utf-8"))
+                   
+        #print dieseMannschaften 
+        #print "1. FC Köln".encode("utf-8")
+        #print "1. FC Köln".decode("utf-8")
+            
+        self.assertIn("1. FC Köln".decode("utf-8"), mannschaften().items()[0][1], "FC Augsburg nicht vorhanden")
