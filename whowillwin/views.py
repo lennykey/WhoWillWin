b6d40c669da7fcd6aa03e2996f9ef9dba6d3c288
JAC <lennykey@gmx.de>
2012-04-09 21:57:12 +0200

    Umzug zu pyDev und Bereinigung von Fehlern, die unter Pydev gefunden wurden. Z.B. Indents mit Tabs.

--- whowillwin/views.py
+++ whowillwin/views.py
@@ -2,6 +2,8 @@ from pyramid.view import view_config
 from suds.client import Client
 from pyramid.response import Response
 
+
+
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project':'whoWillWin'}
@@ -21,14 +23,14 @@ def mannschaften():
     #print teams[0]
     
     #for team in teams.Team:
-	#    print type(team)
-	#    teamName = team.teamName
+    #    print type(team)
+    #    teamName = team.teamName
 
     #    teamNameList.append(teamName)
 
 
     for team in teams.Team:
-        print team.teamName
+        #print team.teamName
         teamNameList.append( team.teamName.encode('utf-8').decode('utf-8') )
 
     #result = []
@@ -60,45 +62,47 @@ def getMatchesForTeam(mannschaft):
     gesuchteMannschaft = mannschaft 
     gesuchteMannschaftTore = 0
     gesuchteMannschaftGegentore = 0
-	
+    
     mannschaftsSpiele = [] 
 
     for match in alleSpiele.Matchdata:
-	if match.nameTeam1 == gesuchteMannschaft or match.nameTeam2 == gesuchteMannschaft:
-   	    mannschaftsSpiele.append(match)
-   	    
+        if match.nameTeam1 == gesuchteMannschaft or match.nameTeam2 == gesuchteMannschaft:
+            mannschaftsSpiele.append(match)
+            print match.nameTeam1
+            print match.nameTeam2
+            
     result = []
     
     #result.append('<ul>')
     
     for begegnung in mannschaftsSpiele:
-	#print type(begegnung)
-	#print begegnung
-	#print unicode(begegnung.nameTeam1)
-	#print begegnung.nameTeam2
+    #print type(begegnung)
+    #print begegnung
+    #print unicode(begegnung.nameTeam1)
+    #print begegnung.nameTeam2
         #print begegnung.matchResults 
 
-	if(begegnung.nameTeam1 == gesuchteMannschaft and (begegnung.pointsTeam1 >= 0 and begegnung.pointsTeam2 >= 0) ):
-	    gesuchteMannschaftTore += begegnung.pointsTeam1 
-	    gesuchteMannschaftGegentore += begegnung.pointsTeam2
-	elif(begegnung.nameTeam1 != gesuchteMannschaft and (begegnung.pointsTeam1 >= 0 and begegnung.pointsTeam2 >= 0) ):
-	    gesuchteMannschaftTore += begegnung.pointsTeam2
-	    gesuchteMannschaftGegentore += begegnung.pointsTeam1
-	   
-        #print 'Tore: ' + str( gesuchteMannschaftTore )
-	#print 'Gegentore: ' + str( gesuchteMannschaftGegentore )
+        if(begegnung.nameTeam1 == gesuchteMannschaft and (begegnung.pointsTeam1 >= 0 and begegnung.pointsTeam2 >= 0) ):
+            gesuchteMannschaftTore += begegnung.pointsTeam1 
+            gesuchteMannschaftGegentore += begegnung.pointsTeam2
+        elif(begegnung.nameTeam2 == gesuchteMannschaft and (begegnung.pointsTeam1 >= 0 and begegnung.pointsTeam2 >= 0) ):
+            gesuchteMannschaftTore += begegnung.pointsTeam2
+            gesuchteMannschaftGegentore += begegnung.pointsTeam1
+        
+    #print 'Tore: ' + str( gesuchteMannschaftTore )
+    #print 'Gegentore: ' + str( gesuchteMannschaftGegentore )
 
-	#dict = {unicode(str(begegnung.nameTeam1)) : unicode(str(begegnung.pointsTeam1)), unicode(str(begegnung.nameTeam2)) : unicode(str(begegnung.pointsTeam2)) }
-	
-	#dict = {str(begegnung.nameTeam1).decode('utf-8') : str(begegnung.pointsTeam1).decode('utf-8'), str(begegnung.nameTeam2).decode('utf-8') : str(begegnung.pointsTeam2).decode('utf-8') }
+    #dict = {unicode(str(begegnung.nameTeam1)) : unicode(str(begegnung.pointsTeam1)), unicode(str(begegnung.nameTeam2)) : unicode(str(begegnung.pointsTeam2)) }
+    #dict = {str(begegnung.nameTeam1).decode('utf-8') : str(begegnung.pointsTeam1).decode('utf-8'), str(begegnung.nameTeam2).decode('utf-8') : str(begegnung.pointsTeam2).decode('utf-8') }
 
 
-	#print type(begegnung.nameTeam1)	
+    #print type(begegnung.nameTeam1)	
 
-	dict = { (begegnung.nameTeam1).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam1).encode('utf-8').decode('utf-8'), (begegnung.nameTeam2).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam2).encode('utf-8').decode('utf-8') }
-        result.append( dict  )
+        mydict = { (begegnung.nameTeam1).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam1).encode('utf-8').decode('utf-8'), (begegnung.nameTeam2).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam2).encode('utf-8').decode('utf-8') }
+        result.append( mydict  )
 
-	
+        print mydict
+    
     #result.append('</ul>')
     #print alleSpiele
 
@@ -122,15 +126,18 @@ def getMatches(request):
 
 @view_config(route_name='whoWillWin', renderer='templates/whowillwin.pt')
 def whoWillWin(request):
-
+    #print "def whoWillWin"
     return {'whowillwin':'you'} 
 
 @view_config(route_name='compareTwoTeamsSeason', renderer='templates/compareTwoTeamsSeason.pt')
 def compareTwoTeamsSeason(request):
-    heimErgebnis = ( float( request.matchdict['heimtore'] ) / float (request.matchdict['gastgegentore'] ) ) * ( float (request.matchdict['heimtore'] ) / float( request.matchdict['spieltag'] ) )    
+    #heimErgebnis = ( float( request.matchdict['heimtore'] ) / float (request.matchdict['gastgegentore'] ) ) * ( float (request.matchdict['heimtore'] ) / float( request.matchdict['spieltag'] ) )    
     
-    gastErgebnis = ( float( request.matchdict['gasttore'] ) / float (request.matchdict['heimgegentore'] ) ) * ( float (request.matchdict['gasttore'] ) / float( request.matchdict['spieltag'] ) )    
+    #gastErgebnis = ( float( request.matchdict['gasttore'] ) / float (request.matchdict['heimgegentore'] ) ) * ( float (request.matchdict['gasttore'] ) / float( request.matchdict['spieltag'] ) )    
     
+    heimErgebnis = ( ( float( request.matchdict['heimtore'] ) / float( request.matchdict['spieltag'] ) ) + float( request.matchdict['gastgegentore'] ) / float( request.matchdict['spieltag'] ) ) / 2 
+    
+    gastErgebnis = ( ( float( request.matchdict['gasttore'] ) / float( request.matchdict['spieltag'] ) ) + float( request.matchdict['heimgegentore'] ) / float( request.matchdict['spieltag'] ) ) / 2 
 
 
     return {'heimmannschaft' : request.matchdict['heim'], 'heimErgebnis': int( round(heimErgebnis) ), 'gastmannschaft': request.matchdict['gast']  , 'gastErgebnis' : int( round(gastErgebnis) ) } 
