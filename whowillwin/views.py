70670f0a44c5ca3d284f00b0f3543e266b36a93a
JAC <lennykey@gmx.de>
2013-03-10 22:21:57 +0100

    Kleinere Aenderungen und Refactorings

--- whowillwin/views.py
+++ whowillwin/views.py
@@ -1,7 +1,10 @@
+"""
+View des whowillwin Tipgenerators
+"""
+
 from pyramid.view import view_config
 from suds.client import Client
 from whowillwin.Team import Team
-#from pyramid.response import Response
 from whowillwin.ArrayOfTeam import ArrayOfTeam
 from whowillwin.Group import Group
 
@@ -9,39 +12,41 @@ from whowillwin.Group import Group
 import logging
 import datetime
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
-def aktuelleSaison():
-    now = datetime.datetime.now()
-    year = now.year
-    month = now.month
-    #month = 3
+def aktuelle_saison():
+    """
+    Gibt das Jahr zurueck, in der die aktuelle Saision gestartet ist
+    """
+    
+    current = datetime.datetime.now()
+    year = current.year
+    month = current.month
 
     if(month < 7):
         year = year - 1
 
-    log.debug("Aktuelles Jahr: %s" % year)
-    log.debug("Aktueller Monat: %s" % month)
+    LOG.debug("Aktuelles Jahr: %s" % year)
+    LOG.debug("Aktueller Monat: %s" % month)
     return year
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
+    """
+    """
     return {'project':'whoWillWin'}
+
 def openLigaWSDLUrl():
     url = "http://www.OpenLigaDB.de/Webservices/Sportsdata.asmx?WSDL"
     return url
 
 def bundesligaMannschaftenOpenLigaDB():
-    year = aktuelleSaison()
+    year = aktuelle_saison()
     client = Client(openLigaWSDLUrl())
     return client.service.GetTeamsByLeagueSaison('bl1', year)
 
-#@view_config(route_name='mannschaften', renderer='templates/mannschaften.pt')
 def mannschaften(client):
-    #url = "http://www.OpenLigaDB.de/Webservices/Sportsdata.asmx?WSDL"
-    #client = Client(url)
-    #myClient = client
     teams = ArrayOfTeam(client.Team)
 
     teamNameList = []
@@ -51,34 +56,10 @@ def mannschaften(client):
         thisTeam = Team(team.teamID, team.teamName, team.teamIconURL)
         teamList.append(thisTeam)
         
-    #logging.info(teams)
-    #logging.info('First logger')
-    #logging.info(str(teams))
-
-    #print teams[0]
-    
-    #for team in teams.ArrayOfTeam:
-    #    print type(team)
-    #    teamName = team.teamName
-
-    #    teamNameList.append(teamName)
-
-
-    #for team in teams.ArrayOfTeam:
     for team in teamList:
-        #print team.teamName
         teamNameList.append( team.teamName )
 
-    #result = []
-    #result.append('<ul>')
 
-    #for element in teamNameList:
-    #    result.append('<li>' + element + '</li>')
-
-    #result.append('</ul>')
-   
-    #return Response( 'Bundesliga Mannschaften ' + str (len( teamNameList ) ) + ' '  + ''.join( result ) ) 
-    #return Response(str(result).decode('utf-8')) 
     return {'teams' : teamNameList }
 
 @view_config(route_name='ulMannschaften', renderer='templates/ulMannschaften.pt')
@@ -96,7 +77,7 @@ def getMatchesForTeam(mannschaft):
     '''
     url = 'http://www.openligadb.de/Webservices/Sportsdata.asmx?WSDL'
     client = Client(url)
-    year = aktuelleSaison()
+    year = aktuelle_saison()
     alleSpiele = client.service.GetMatchdataByLeagueSaison('bl1', year)
 
     gesuchteMannschaft = mannschaft 
@@ -108,19 +89,13 @@ def getMatchesForTeam(mannschaft):
     for match in alleSpiele.Matchdata:
         if match.nameTeam1 == gesuchteMannschaft or match.nameTeam2 == gesuchteMannschaft:
             mannschaftsSpiele.append(match)
-            log.debug(match.nameTeam1)
-            log.debug(match.nameTeam2)
+            LOG.debug(match.nameTeam1)
+            LOG.debug(match.nameTeam2)
             
     result = []
     
-    #result.append('<ul>')
     
     for begegnung in mannschaftsSpiele:
-    #print type(begegnung)
-    #print begegnung
-    #print unicode(begegnung.nameTeam1)
-    #print begegnung.nameTeam2
-        #print begegnung.matchResults 
 
         if(begegnung.nameTeam1 == gesuchteMannschaft and (begegnung.pointsTeam1 >= 0 and begegnung.pointsTeam2 >= 0) ):
             gesuchteMannschaftTore += begegnung.pointsTeam1 
@@ -129,35 +104,14 @@ def getMatchesForTeam(mannschaft):
             gesuchteMannschaftTore += begegnung.pointsTeam2
             gesuchteMannschaftGegentore += begegnung.pointsTeam1
         
-    #print 'Tore: ' + str( gesuchteMannschaftTore )
-    #print 'Gegentore: ' + str( gesuchteMannschaftGegentore )
-
-    #dict = {unicode(str(begegnung.nameTeam1)) : unicode(str(begegnung.pointsTeam1)), unicode(str(begegnung.nameTeam2)) : unicode(str(begegnung.pointsTeam2)) }
-    #dict = {str(begegnung.nameTeam1).decode('utf-8') : str(begegnung.pointsTeam1).decode('utf-8'), str(begegnung.nameTeam2).decode('utf-8') : str(begegnung.pointsTeam2).decode('utf-8') }
-
-
-    #print type(begegnung.nameTeam1)	
 
         mydict = { (begegnung.nameTeam1).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam1).encode('utf-8').decode('utf-8'), (begegnung.nameTeam2).encode('utf-8').decode('utf-8') : str(begegnung.pointsTeam2).encode('utf-8').decode('utf-8') }
         result.append( mydict  )
 
-        #print mydict
-    
-    #result.append('</ul>')
-    #print alleSpiele
 
     returnDict  = {'GesuchteMannschaft': gesuchteMannschaft , 'Tore': str(gesuchteMannschaftTore), 'Gegentore': str(gesuchteMannschaftGegentore), 'Spiele':  result   }
     
-    #print str((returnDict['Spiele'][3]).keys()[0]).decode('utf-8', 'replace') 
-
-    #return {'project': 'cool'}
     return returnDict  
-    #return str( list(returnDict.values()[3][3])[0] )
-    #return Response( 'Bundesliga Spiele:\n ' +
-#		'\nAnzahl Tore: ' + str(gesuchteMannschaftTore) +		
-#		'\nAnzahl Gegentore: ' + str(gesuchteMannschaftGegentore) +		
-#		''.join( result )
-#		)
 
 
 @view_config(route_name='matchesfor', renderer='templates/getmatches.pt')
@@ -169,16 +123,12 @@ def getMatches(request):
 def whoWillWin(request):
     group = currentgroup()
     aktuellerspieltag = aktuellerSpieltag(group)
-    log.debug('Aktuelle Saison: %s' % aktuelleSaison())
-    #log.debug("Aktueller Spieltag %s" % aktuellerspieltag)
-    #print "def whoWillWin"
+    LOG.debug('Aktuelle Saison: %s' % aktuelle_saison())
+
     return {'whowillwin':'you', 'aktuellerSpieltag': aktuellerspieltag } 
 
 @view_config(route_name='compareTwoTeamsSeason', renderer='templates/compareTwoTeamsSeason.pt')
 def compareTwoTeamsSeason(request):
-    #heimErgebnis = ( float( request.matchdict['heimtore'] ) / float (request.matchdict['gastgegentore'] ) ) * ( float (request.matchdict['heimtore'] ) / float( request.matchdict['spieltag'] ) )    
-    #gastErgebnis = ( float( request.matchdict['gasttore'] ) / float (request.matchdict['heimgegentore'] ) ) * ( float (request.matchdict['gasttore'] ) / float( request.matchdict['spieltag'] ) )    
-    
     heimErgebnis = ( ( float( request.matchdict['heimtore'] ) / float( request.matchdict['spieltag'] ) ) + float( request.matchdict['gastgegentore'] ) / float( request.matchdict['spieltag'] ) ) / 2 
     
     gastErgebnis = ( ( float( request.matchdict['gasttore'] ) / float( request.matchdict['spieltag'] ) ) + float( request.matchdict['heimgegentore'] ) / float( request.matchdict['spieltag'] ) ) / 2 
@@ -189,14 +139,13 @@ def compareTwoTeamsSeason(request):
 def currentgroup(): 
     client = Client(openLigaWSDLUrl())
     currentGroup = client.service.GetCurrentGroup('bl1')
-    log.debug(currentGroup)
+    LOG.debug(currentGroup)
     return currentGroup
 
 def aktuellerSpieltag(group):
     group = Group(group.groupName, group.groupOrderID, group.groupID)
-    #return group.groupName, group.groupID, group.groupOrderID
-    log.debug(group.groupName)
-    log.debug(group.groupOrderID)
-    log.debug(group.groupID)
+    LOG.debug(group.groupName)
+    LOG.debug(group.groupOrderID)
+    LOG.debug(group.groupID)
     return group.groupOrderID
 
